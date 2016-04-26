4a610a7bce83cfc75a853099fe92f030bff3c7ca
PaulineOC <poc215@nyu.edu>
2016-04-25 20:56:44 -0400

    adding initial Bayes

---
+++ Bayes.py
@@ -0,0 +1,21 @@
+
+articles_total = 5006
+class_freqs = [615, 1553, 1998, 840]
+priors = [w / float(5006) for w in class_freqs]
+word_counts = {}
+
+
+
+file words_classified = open("words_classified.txt", "r");
+##str all_input = words.classified.read();
+
+for line in words_classified:
+    
+
+
+def BC_word(){
+    num = word
+
+
+
+    }
